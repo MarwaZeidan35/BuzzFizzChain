f97c48dd2e14d3b3594f38a6bee3526a344f5cd9
MarwaZeidan35 <marwa.b.zeidan@gmail.com>
2015-09-29 22:15:13 +0200

    Create quiz2.java

---
+++ quiz2.java
@@ -0,0 +1,34 @@
+public class quiz2
+{
+    public static void main( String... args )
+    {
+        int num = 100;
+        for (int i = 1; i <= num; i++)
+        {
+            boolean flag = false;
+
+            if (i % 3 == 0)
+            {
+                flag = true;
+                System.out.print("Fizz");
+            }
+           else if (i % 5 == 0)
+            {
+                flag = true;
+                System.out.print("Buzz");
+            }
+            
+        else  if( (i % 5 == 0) && (i % 3 == 0) )
+            {
+                flag = true;
+                System.out.print("Buzz");
+            }
+
+            
+
+            if (flag == false) 
+            System.out.print(i);
+            System.out.println();
+        }
+    }
+}
